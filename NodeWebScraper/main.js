9d223e2ffce38f593338474768d1d023f0ddf3e0
Matt Gibbs <mtgibbs21@gmail.com>
2014-08-27 01:56:50 -0400

    Adding in the timers.
    They are default set to five seconds.  Not really a problem since no
    action is taken other than a console log currently.

--- NodeWebScraper/main.js
+++ NodeWebScraper/main.js
@@ -1,11 +1,21 @@
 ﻿var CowboomScraper = require('./scraper/CowboomScraper').Scraper.CowboomScraper;
 var MehScraper = require('./scraper/MehScraper').Scraper.MehScraper;
 
-var boom = new CowboomScraper('ps4');
-boom.scrape();
+var scrapers = [];
+scrapers.push(new CowboomScraper('ps4'));
+scrapers.push(new MehScraper());
 
-var meh = new MehScraper();
-meh.scrape();
+scrapers.forEach(function (scraper) {
+    try {
+        setInterval(function () {
+            scraper.scrape();
+        }, 5000);
+    } catch (error) {
+        // TODO: Need to find a logging library...
+        console.log(error.message);
+        console.log('error occured while trying start timer for [' + JSON.stringify(scraper) + ']');
+    }
+});
 
 require('readline')
     .createInterface(process.stdin, process.stdout)
