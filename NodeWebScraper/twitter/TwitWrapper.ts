e00d9a75cda0df8b6b6a77089369401e9dd32f44
Matt Gibbs <mtgibbs21@gmail.com>
2014-08-29 00:22:04 -0400

    Building in twitter support.
    Imported twit module.

---
+++ NodeWebScraper/twitter/TwitWrapper.ts
@@ -0,0 +1,40 @@
+﻿/// <reference path="../typings/node.d.ts"/>
+/// <reference path="../typings/twit.d.ts"/>
+
+var Twit = require('twit');
+
+export module Twitter {
+
+    export class TwitWrapper {
+
+        private _atUser: string;
+        private _twit: Twit;
+
+        constructor(username: string);
+        constructor(username: string, twitOptions: TwitOptions);
+
+        constructor(username: string, twitOptions?: TwitOptions) {
+
+            if (typeof twitOptions === 'undefined' || twitOptions == null) {
+                twitOptions = require('./../twitter.json');
+            }
+            this._twit = new Twit(twitOptions);
+            this._atUser = username;
+        }
+
+        public tweet(message: string) {
+            var message = '@' + this._atUser + ' ' + message;
+            message = message.substr(0, 140);
+
+            this._twit.post('statuses/update', { status: message }, (err, data, response) => {
+                if (!err) {
+                    console.log('Message tweeted @' + this._atUser);
+                } else {
+                    console.log(err);
+                }
+            });
+        }
+    }
+}
+
+exports.TwitWrapper = Twitter.TwitWrapper; 
---
+++ NodeWebScraper/typings/twit.d.ts
@@ -0,0 +1,12 @@
+﻿/// <reference path="node.d.ts" />
+
+interface TwitOptions {
+    consumer_key: string;
+    consumer_secret: string;
+    access_token: string;
+    access_token_secret: string;
+}
+
+declare class Twit {
+    post(type: string, config: any, callback: any);
+}
